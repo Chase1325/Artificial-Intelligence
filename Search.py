8693b2bccdf5380c5485432d0ee9bd695ccc06ca
clstlaurent <clstlaurent@folivoraapps.com>
2019-01-21 16:05:34 -0500

    Search initialization

---
+++ Search.py
@@ -0,0 +1,40 @@
+import argparse
+
+#Depth 1st Search
+def depthFirst():
+    pass
+
+#Breadth 1st Search
+def breadthFirst():
+    pass
+
+#Depth-Limited Search w/ depth-limit=2
+def depthLimited():
+    pass
+
+#Iterative Deepening Search (All iterations shown)
+def iterativeDeep():
+    pass
+
+#Uniform Cost Search a.k.a Branch-and-Bound
+def uniformCost():
+    pass
+
+#Greedy Search a.k.a Best 1st Search
+def greedy():
+    pass
+
+#A*
+def aStar():
+    pass
+
+#Hill-climbinging without backtracking
+def hillClimbNoBacktrack():
+    pass
+
+#Beam search with width=2
+def beam():
+    pass
+
+if name== "__main__":
+    pass
